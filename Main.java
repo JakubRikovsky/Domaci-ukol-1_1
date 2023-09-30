c7ab438a11f71255753431364096a6f41237af60
JakubRikovsky <145039678+JakubRikovsky@users.noreply.github.com>
2023-09-30 09:48:04 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,75 @@
+import javax.print.DocFlavor;
+import java.math.BigDecimal;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+    public class Main {
+
+   public static void ukol1() {
+        System.out.println("Hello world!"); // chybí uvozovky a strednik na konci
+    }
+    public static void ukol2() {
+        String uzivatel = "Karel";  // chybely uvozovky
+        System.out.println(uzivatel);
+    }
+
+   public static void ukol3() {
+        int pocetLekci = 10;   // místo string pro cislo pouziji "int"
+        System.out.println(pocetLekci);
+    }
+
+    public static void ukol4() {
+        double cena = 10.0; // carka misto tecky 10.0
+        System.out.println(cena);
+    }
+
+    public static void ukol5() {
+        BigDecimal cena = BigDecimal.ZERO; // Nastavíme do proměnné hodnotu 0 // použiji Bigdecimal s nastaveni ZERO
+        // Desetkrát přičteme hodnotu jedna desetina (0,1):
+        for (int i = 0; i < 10; i++) {
+            cena = cena.add(BigDecimal.valueOf(0.1)); // přenastavím cenu na cenu s Bigdecimal
+               // operátor "+=" znamená: přičti ke stávající
+               // hodnotě navíc 0.1
+        }
+        System.out.println(cena);
+        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
+    }
+
+   public static void ukol6() {
+       BigDecimal cena = BigDecimal.ZERO;
+       cena = cena.add(BigDecimal.valueOf(0.1));
+      System.out.println(cena);
+   }
+
+   public static void ukol7() {
+       int vykonMotoru = 120; // odstraním "" abych měl číslo
+        System.out.println("Výkon Motoru je: "+vykonMotoru+" KW. "); // pridám + pro text
+    }
+
+    public static void ukol8() {
+        int velikostKosile = 37;
+       System.out.println("Velikost košile je: "+velikostKosile+".");
+
+   }
+
+    public static void ukol9() {
+        System.out.println("Metoda *skoro* bez chybičky!");
+   }
+
+      public static void main(String[] args) {
+            System.out.println("Oprav chyby a spusť všechny metody!");
+           System.out.println("Komentáře odstraníš klávesovou"
+                    +"zkratkou <Ctrl>+</> - použij lomítko "
+                    +"na numerické klávesnici.");
+
+        ukol1();
+        ukol2();
+        ukol3();
+        ukol4();
+        ukol5();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
+        }
+    }
